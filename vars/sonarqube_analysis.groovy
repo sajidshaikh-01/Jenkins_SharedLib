f77d96b9efbfa3de0369fb273f76418b5fb87eeb
Sajid Shaikh <sajid0100shaikh@gmail.com>
2025-10-26 15:42:54 +0530

    Refactor SonarQube analysis script for improved command

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,12 +1,5 @@
 def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeAPI}") {
-      sh '''
-        echo "Starting SonarQube analysis..."
-        sonar-scanner \
-          -Dsonar.projectKey=${ProjectKey} \
-          -Dsonar.projectName=${Projectname} \
-          -Dsonar.sources=. \
-          -Dsonar.java.binaries=target/
-      '''
+  withSonarQubeEnv("${SonarQubeAPI}"){
+      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
   }
 }
