f1047c287dddca307f972147026ffafe29731544
Sajid Shaikh <sajid0100shaikh@gmail.com>
2025-10-26 15:58:18 +0530

    Increase timeout for quality gate check to 10 minutes
    Increased timeout duration for quality gate check from 1 minute to 10 minutes and added error handling for failed quality gates.

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,5 +1,10 @@
 def call(){
-  timeout(time: 1, unit: "MINUTES"){
-      waitForQualityGate abortPipeline: false
-  }
+    timeout(time: 10, unit: "MINUTES"){   // increased from 1 to 10 minutes
+        def qg = waitForQualityGate abortPipeline: true
+        if (qg.status != 'OK') {
+            error "❌ Pipeline aborted due to Quality Gate failure: ${qg.status}"
+        } else {
+            echo "✅ Quality Gate passed: ${qg.status}"
+        }
+    }
 }
